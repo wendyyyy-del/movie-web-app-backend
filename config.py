daf42be317f1e15f975aef899522f3958fbbdd4f
george <george.benedict@student.moringaschool.com>
2025-06-26 17:24:31 +0300

    modifications

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
